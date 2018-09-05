5bc687341d247e5166e0f03f8f5a138427f11e58
Vaibhav Shukla <cse.vaibhavshukla@gmail.com>
2018-09-04 23:36:08 -0700

    connected to database , and inserted the value in the mongo db

--- test.js
+++ test.js
@@ -1,6 +1,57 @@
 // request is a module that makes http calls easier
 const request = require('request');
+const MongoClient =  require('mongodb').MongoClient;
 
+const dsn = "mongodb://localhost:27017";
+MongoClient.connect(dsn, {useNewUrlParser : true} , (err , client) => {
+    console.time('mongodb');
+    if(err){
+        console.log(err);
+    }
+
+    console.log("Db connected");
+    fetchFromAPI((err, data) => {
+        try{
+            if (err) throw err;
+
+            const db = client.db('maxcoin');
+
+            const collection =  db.collection('value');
+            insertMongodb(collection , data.bpi).then((result) =>{
+                console.log(`Inserted sucessfully`);
+                const options = {sort : [['value' , 'desc']]};
+                collection.findOne({} , options  , (err , doc) => {
+                    if(err) throw err ;
+
+                    console.log(`Max value reach ${doc.value} on date ${doc.date}`);
+                    client.close();
+                    console.timeEnd('mongodb');
+                })
+               
+            }).catch((err) => {
+                console.log(err);
+                process.exit();
+            });
+        }catch(err){
+            console.log(err);
+        }
+      
+    });
+    
+});
+
+function insertMongodb(collection , data ){
+    const promiseInserts = [] ;
+    Object.keys(data).forEach((element) => {
+        promiseInserts.push(
+            collection.insertOne({
+                date : element , 
+                value : data[element]
+            })
+        )
+    });
+    return Promise.all(promiseInserts);
+}
 // Generic function that fetches the closing bitcoin dates of the last month from a public API
 function fetchFromAPI(callback) {
 
@@ -11,7 +62,3 @@ function fetchFromAPI(callback) {
     });
 }
 
-fetchFromAPI((err, data) => {
-    if (err) throw err;
-    console.log(data);
-});
